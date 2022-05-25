1a5dca62874b9799ef2fbbd8e0a06f9b601ecdc5
Ella Svete <ella.svete@gmail.com>
2022-05-25 11:58:13 -0700

    setup file with demo

---
+++ snacks/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Snack
+
+# Create your views here.
+
+class SnackListView(ListView):
+  template_name = "snack_list.html"
+  model = Snack
+
+
+class SnackDetailView(DetailView):
+  template_name = "snack_detail.html"
+  model = Snack
+
+
+class SnackCreateView(CreateView):
+  template_name = "snack_create.html"
+  model = Snack
+  fields = ["title", "purchaser"]
+
+class SnackUpdateView(UpdateView):
+  template_name = 'snack_update.html'
+  model = Snack
+  fields = ["title", "description", "purchaser"]
